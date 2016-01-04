34f8c5010609655833990395b284782ddda4ecad
plutoshe <plutoshe@gmail.com>
2016-01-04 16:46:39 +0800

    test: add bson convert test part

---
+++ bson_convert_test.go
@@ -0,0 +1,109 @@
+package bson_convert
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+func TestConvertToBSON(t *testing.T) {
+	type substructure struct {
+		E string `bson:"e,omitempty"`
+		F []int  `bson:"f,omitempty"`
+	}
+	type testStructure struct {
+		A int          `bson:"a,omitempty"`
+		B string       `bson:"b,omitempty"`
+		C []int        `bson:"c,omitempty"`
+		D substructure `bson:"d"`
+	}
+
+	testcases := []struct {
+		source   testStructure
+		omitted  bool
+		required []string
+		ignored  []string
+		res      bson.M
+	}{
+		{
+			testStructure{
+				3,
+				"BBB",
+				[]int{32, 123, 32},
+				substructure{"3", []int{3, 2}},
+			},
+			false,
+			[]string{},
+			[]string{},
+			bson.M{"a": 3, "b": "BBB", "c": []int{32, 123, 32}, "d": bson.M{"e": "3", "f": []int{3, 2}}},
+		},
+		{
+			testStructure{
+				3,
+				"BBB",
+				[]int{32, 123, 32},
+				substructure{"3", []int{3, 2}},
+			},
+			true,
+			[]string{},
+			[]string{},
+			bson.M{"a": 3, "b": "BBB", "c": []int{32, 123, 32}, "d": bson.M{"e": "3", "f": []int{3, 2}}},
+		},
+		{
+			testStructure{
+				0,
+				"",
+				[]int{32, 123, 32},
+				substructure{"", []int{}},
+			},
+			true,
+			[]string{},
+			[]string{},
+			bson.M{"c": []int{32, 123, 32}, "d": bson.M{}},
+		},
+		{
+			testStructure{
+				0,
+				"",
+				[]int{32, 123, 32},
+				substructure{"", []int{}},
+			},
+			true,
+			[]string{"a", "b", "e"},
+			[]string{},
+			bson.M{"a": 0, "b": "", "c": []int{32, 123, 32}, "d": bson.M{"e": ""}},
+		},
+		{
+			testStructure{
+				3,
+				"BBB",
+				[]int{32, 123, 32},
+				substructure{"3", []int{3, 2}},
+			},
+			false,
+			[]string{},
+			[]string{"c", "d"},
+			bson.M{"a": 3, "b": "BBB"},
+		},
+		{
+			testStructure{
+				0,
+				"BBB",
+				[]int{32, 123, 32},
+				substructure{"3", []int{3, 2}},
+			},
+			false,
+			[]string{"a"},
+			[]string{"c", "e"},
+			bson.M{"a": 0, "b": "BBB", "d": bson.M{"f": []int{3, 2}}},
+		},
+	}
+	for i, v := range testcases {
+		res := Convert2BSON(v.source, v.omitted, v.required, v.ignored)
+		if !reflect.DeepEqual(v.res, res) {
+			fmt.Printf("#%d: Convert result is wrong! Want = %v, Get = %v.\n", i, v.res, res)
+		}
+	}
+}
