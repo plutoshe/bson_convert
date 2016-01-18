3f1e1e03aaba847755c83c349b2e02d35b9c8847
plutoshe <plutoshe@gmail.com>
2016-01-19 01:58:22 +0800

    bson: add time.Time specific Type judgement
    Beside, supplement lack bson name field filling.

--- bson_convert.go
+++ bson_convert.go
@@ -3,8 +3,13 @@ package bson_convert
 import (
 	"reflect"
 	"strings"
+	"time"
 
 	"gopkg.in/mgo.v2/bson"
+)
+
+var (
+	typeTime = reflect.TypeOf(time.Time{})
 )
 
 type bsonPattern struct {
@@ -35,6 +40,9 @@ func (b *bsonPattern) ConvertToBSON(source interface{}) bson.M {
 	for i := 0; i < v.NumField(); i++ {
 		if sourceType.Field(i).Name[0] >= 'A' && sourceType.Field(i).Name[0] <= 'Z' {
 			tag := sourceType.Field(i).Tag.Get("bson")
+			if tag == "" {
+				tag = strings.ToLower(sourceType.Field(i).Name)
+			}
 			fields := strings.Split(tag, ",")
 			if len(fields) > 1 {
 				tag = fields[0]
@@ -52,8 +60,8 @@ func (b *bsonPattern) ConvertToBSON(source interface{}) bson.M {
 }
 
 func (b *bsonPattern) getValue(v reflect.Value) interface{} {
-	if v.Kind() == reflect.Map || v.Kind() == reflect.Struct {
-		return b.ConvertToBSON(v.Interface())
+	if v.Type() != reflect.TypeOf(time.Time{}) && (v.Kind() == reflect.Map || v.Kind() == reflect.Struct) {
+		return b.ConvertToBSON(v)
 	}
 	return v.Interface()
 }
@@ -72,6 +80,20 @@ func isEmptyValue(v reflect.Value) bool {
 		return v.Float() == 0
 	case reflect.Interface, reflect.Ptr:
 		return v.IsNil()
+	case reflect.Struct:
+		vt := v.Type()
+		if vt == typeTime {
+			return v.Interface().(time.Time).IsZero()
+		}
+		for i := 0; i < v.NumField(); i++ {
+			if vt.Field(i).PkgPath != "" {
+				continue // Private field
+			}
+			if !isEmptyValue(v.Field(i)) {
+				return false
+			}
+		}
+		return true
 	}
 	return false
 }
